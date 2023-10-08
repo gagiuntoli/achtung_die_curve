b17269675ec27bfb4c07ec9731d0b03d26be2e86
Guido Giuntoli <gagiuntoli@gmail.com>
2023-10-08 19:56:23 +0200

    Fix unit test

--- physics_test.py
+++ physics_test.py
@@ -1,8 +1,8 @@
-from physics import distance
+from physics import distance2
 
 TOL = 1.0e-10
 
 def test_distance():
-    assert(abs(distance([0, 0], [1, 1]) - 2.0) < TOL)
-    assert(abs(distance([0, 0], [0, 0]) - 0.0) < TOL)
-    assert(abs(distance([2, 0], [0, 0]) - 4.0) < TOL)
+    assert(abs(distance2([0, 0], [1, 1]) - 2.0) < TOL)
+    assert(abs(distance2([0, 0], [0, 0]) - 0.0) < TOL)
+    assert(abs(distance2([2, 0], [0, 0]) - 4.0) < TOL)
